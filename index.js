0183c698c9577f402b5e61753d888d1dbe0fb578
Ravi Singh <116999956+RaviSingh48@users.noreply.github.com>
2025-01-27 10:28:51 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Timer from './component/Timer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Timer/>
+  </React.StrictMode>
+  
+);
+
+
